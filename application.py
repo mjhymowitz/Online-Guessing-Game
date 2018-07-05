772006d8f9cf3c41197714512e73ba4271a6abf7
mhymowitz <9580326+mjhymowitz@users.noreply.github.com>
2018-07-05 12:06:48 -0700

    Add files via upload

--- application.py
+++ application.py
@@ -3,7 +3,6 @@ import random
 import os
 
 app = Flask(__name__)
-
 app.secret_key = 'super secret key'
 
 not_generated = True
@@ -59,7 +58,7 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
     if session.get('randnum') is None:
-        session['win'] = 0
-        session['loss'] = 0
-        session['mistakes'] = 0
-        session['randnum'] = 0
+        session.get('win') = 0
+        session.get('loss') = 0
+        session.get('mistakes') = 0
+        session.get('randnum') = 0
